000f15ebe1264916b1f006e7952685c1ff6d9f14
layik <layikrumeysa@gmail.com>
2021-10-05 00:32:02 +0300

    Product Index page is created

--- Shop.WebUI/Controllers/ProductController.cs
+++ Shop.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Business.Abstract;
+using Shop.Entities;
+using Shop.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,23 @@ namespace Shop.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = new CatalogListModel()
+            {
+                Categories = unitOfWork.Categories.GetAll().ToList(),
+                Products = unitOfWork.Products.GetAll().ToList()
+            };
+
+            return View(model);
         }
+
+
     }
 }
