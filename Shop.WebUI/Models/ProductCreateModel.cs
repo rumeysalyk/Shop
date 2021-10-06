68dfab6a61c133c3b578883b4d1e156612067095
layik <layikrumeysa@gmail.com>
2021-10-06 12:02:16 +0300

    ProductCreateModel and createview are created

---
+++ Shop.WebUI/Models/ProductCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Shop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Shop.WebUI.Models
+{
+    public class ProductCreateModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int SelectedCategoryId { get; set; }
+    }
+
+
+
+}
